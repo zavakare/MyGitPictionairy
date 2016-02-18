da586d347f4c377137c0c70e0a85308c9f3fb3eb
Mary Lee <leemary@my.dom.edu>
2016-02-17 21:00:13 -0600

    initial set up of camera and SimpleCv

---
+++ Motion1.py
@@ -0,0 +1,21 @@
+#Camera for pictionAIRy
+#2/17/16
+#Mary/ Alex/ Sam/ Karen/ Theodora
+
+# import simplecv and camera
+from SimpleCV import Camera
+
+#initialize camera
+cam = Camera()
+
+# Loop to continuously get images
+
+while True:
+
+    # Get Image from camera
+
+    img = cam.getImage()
+
+    # Show the image
+
+    img.show()
