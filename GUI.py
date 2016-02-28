71ca03a8692721a4085db464244a0f99fbe84bee
Mary Lee <leemary@my.dom.edu>
2016-02-28 15:20:03 -0600

    Added Rules Page- Needs Formatting

--- GUI.py
+++ GUI.py
@@ -4,7 +4,6 @@ from Tkinter import *
 
 #create blank window
 root = Tk()
-
 root.minsize(1900,1080)
 root.configure(bg =  "Purple" )
 root.title("The Reflections + Alex presents: pictionAIRy")
@@ -17,10 +16,29 @@ title.pack()
 
 def closewindow():
 	exit()
+def rulesExit():
+	rulesWin.exit()
+
 
 def listrules():
-	print "Rules listed"
-
+	rulesWin = Tk()
+	rulesWin.minsize(1200,800)
+	RuleHeader = Label(rulesWin, text = 'Rules Listed')
+	RuleHeader.pack()
+	Rule1 = Label(rulesWin, text = '1.Separate into 2 teams, each team consisting of 2 people, and enter your team names. ')
+	Rule1.pack()
+	Rule2 = Label(rulesWin, text = '2.Pick 1 player to begin and choose your drawing category. ')
+        Rule2.pack()
+	Rule3 = Label(rulesWin, text = '3.Player 1 gets word to draw and gets tracking device to put on the dominate hand. ')
+        Rule3.pack()
+	Rule4 = Label(rulesWin, text = '4.Stand in front of web cam and draw. You will have 45 seconds to draw this.  ')
+        Rule4.pack()
+	Rule5 = Label(rulesWin, text = '5.You will have unlimited unoffical guesses and one offical guess.  You will offically guess by hitting the buzzer and saying your answer out loud. ')
+        Rule5.pack()
+	Rule6 = Label(rulesWin, text = '6.The team will get the point if the guess is correct.  The Team at the end of 3 rounds with the most points will win! ')
+        Rule6.pack()
+	rulesButton = Button(rulesWin, text="Go BaCk", command=closewindow, font=("Georgia", 40))
+	rulesButton.pack()
 #creates buttons
 button = Button(root, text="Exit", command=closewindow, font=("Georgia", 40))
 button2 = Button(root, text="Rules", command=listrules, font=("Georgia", 40))
