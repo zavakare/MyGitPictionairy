242dd5774499d95d92d705606e9919e086f1ddc8
Sam <tracsama@my.dom.edu>
2016-03-01 14:55:14 -0600

    Lines are drawn inbetween tracked points. Documentation also added

--- NewMotion.py
+++ NewMotion.py
@@ -1,41 +1,45 @@
 #!/usr/bin/python
 from SimpleCV import *
 
-
+#Defines camera and resolution
 cam = Camera(prop_set = {"width":320,"height":240})
 
 size = cam.getImage().size()
 disp = Display(size)
 
+#creates array where tracked center coordinates will be stored
 lot=[]
 
 #myDrawingLayer = DrawingLayer((320,240))
 #myDrawingLayer.setDefaultColor("Black")
 
 while True:
-
+	#get image from camera
 	img = cam.getImage().flipHorizontal()
+	#want to track object whose color is furthest distance from black
 	dist = img.colorDistance(Color.BLACK).dilate(2)
 
 	segmented = dist.stretch(200,255)
-
+	#find blobs in image
 	blobs = segmented.findBlobs()
 
 	if blobs:
-
+		#from the blobs which have been found, find circles
 		circles = blobs.filter([b.isCircle(0.2) for b in blobs])
-		print circles.coordinates()
 		
 		if circles:
 			lot.append((circles[-1].x, circles[-1].y))
+			#outline the found circle in blue on the screen
 			img.drawCircle((circles[-1].x, circles[-1].y), circles[-1].radius(),Color.BLUE,3)
-			#img.dl().circle((circles[-1].x, circles[-1].y), 6, Color.BLACK, width = 1, filled=True)
-	for (x,y) in lot:
-		img.dl().circle((x,y), 6, Color.BLACK, width = 2, filled=True)
-#	for i in range(len(lot))
-#		a, b=lot[i]
-#		c, d=lot[i-1]
-#		img.dl().line(start=
+	#as long as there is more than 1 point in the list, draw lines between the points
+	if (len(lot)>1):
+		img.dl().lines(lot, color=(0,0,0), antialias=True, alpha=-1, width=5)
+	
+	#draws circles at the points in lot
+#	for (x,y) in lot:
+#		img.dl().circle((x,y), 6, Color.BLACK, width = 2, filled=True)
+
 	#img.addDrawingLayer(myDrawingLayer)
 	#img.applyLayers()
+	#display image (camera picture) on the screen
 	img.show()
