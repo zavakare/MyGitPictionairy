016e96af292f123dae8e5cf72329b80c6c085ce7
Karen <zavakare@my.dom.edu>
2016-04-02 06:11:04 +0000

    Enhanced the GUIs' first page and added some images

--- GUI.py
+++ GUI.py
@@ -1,12 +1,36 @@
 #!/usr/bin/env python 
 import startGame 
 from Tkinter import *
+from PIL import Image, ImageTk
+
 
 #creates GUI interface, begins game
 
 #create blank window and set minimum size
 root = Tk()
 root.minsize(1780, 1080)
+
+
+#Opening the images
+imgTitle = Image.open("pictionary.gif")
+render = ImageTk.PhotoImage(imgTitle)
+imgStartB = Image.open("StartButton.gif")
+renderStart = ImageTk.PhotoImage(imgStartB)
+imgRulesB = Image.open("RulesButton.gif")
+renderRules = ImageTk.PhotoImage(imgRulesB)
+imgAboutB = Image.open("AboutUsButton.gif")
+renderAbout = ImageTk.PhotoImage(imgAboutB)
+imgExitB = Image.open("ExitButton.gif")
+renderExit = ImageTk.PhotoImage(imgExitB)
+
+
+
+
+#Using Label to display our imgTitle
+img = Label(root, image=render,bg =  "Purple" )
+img.image = render
+img.place(x=400,y=20)
+
 
 #change background color
 root.configure(bg =  "Purple" )
@@ -15,8 +39,10 @@ root.configure(bg =  "Purple" )
 root.title("The Reflections + Alex presents: pictionAIRy")
 
 #create title text and default place on screen
-title = Label(root, text='pictionAIRy', font=("Helvetica", 76), fg="Black", bg = "Purple" )
+title = Label(root, font=("Helvetica", 76), fg="Black", bg = "Purple" )
 title.pack()
+
+
 
 #calls method in startGame.py to start the team creation
 def openGame():
@@ -28,6 +54,7 @@ def closewindow():
 
 def rulesExit():
 	rulesWin.exit()
+
 
 #Rules window
 def listrules():
@@ -65,22 +92,22 @@ def aboutus():
         AboutUsText.pack()
 
 #creates main menu  buttons
-button = Button(root, text="Exit", command=closewindow, font=("Georgia", 40))
-button2 = Button(root, text="Rules", command=listrules, font=("Georgia", 40))
-button3 = Button(root, text="Start", command=openGame, font=("Georgia", 40))
-button4 = Button(root, text="About Us", command=aboutus, font=("Georgia", 40))
+button = Button(root, image=renderExit, command=closewindow, bg="Purple")
+button2 = Button(root, image=renderRules, command=listrules, bg="Purple")
+button3 = Button(root, image=renderStart,command=openGame,bg="Purple")
+button4 = Button(root, image=renderAbout, command=aboutus, bg="Purple")
 
 #size of buttons
-button.config(height=3, width=7)
-button2.config(height=3, width=7)
-button3.config(height=3, width=7)
-button4.config(height=3, width=7)
+button.config(height=200, width=400)
+button2.config(height=200, width=400)
+button3.config(height=200, width=400)
+button4.config(height=200, width=400)
 
 #placing of button
-button.place(x=920, y=520)
-button3.place(x=520, y=320)
+button.place(x=420, y=620)
+button3.place(x=420, y=320)
 button2.place(x=920, y=320)
-button4.place(x=520, y=520)
+button4.place(x=920, y=620)
 
 #display window on screen
 root.mainloop()
